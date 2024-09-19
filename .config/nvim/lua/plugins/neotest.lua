c39731bf0e2fb33c88927cbb70256569a6a88f5d
Txart <iurzainki@gmail.com>
2024-09-19 09:27:56 +0200

    minor changes in neotest keybindings

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -11,16 +11,26 @@ return {
 	keys = {
 		{
 			"<leader>tt",
+			':lua require("neotest").run.run(vim.fn.getcwd())<CR>',
+			desc = "Run all project tests with neotest",
+		},
+		{
+			"<leader>tf",
 			':lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
 			desc = "Run current tests file with neotest",
 		},
-		{
-			"<leader>tp",
-			':lua require("neotest").run.run(vim.fn.getcwd())<CR>',
-			desc = "Run tests in project with neotest",
-		},
 		{ "<leader>ts", ":Neotest summary<CR>", desc = "Show test summary" },
 		{ "<leader>to", ":Neotest output<CR>", desc = "Show test output pane" },
+		{
+			"[t",
+			':lua require("neotest").jump.prev({ status = "failed" })<CR>',
+			desc = "Jump to previous failing test",
+		},
+		{
+			"]t",
+			':lua require("neotest").jump.next({ status = "failed" })<CR>',
+			desc = "Jump to next failing test",
+		},
 	},
 	config = function()
 		require("neotest").setup({
