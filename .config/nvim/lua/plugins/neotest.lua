fc229fc4e58e432503c1f9f7a2310dea7d58a682
Txart <iurzainki@gmail.com>
2024-09-19 08:17:09 +0200

    added keymaps to neotest

--- .config/nvim/lua/plugins/neotest.lua
+++ .config/nvim/lua/plugins/neotest.lua
@@ -8,10 +8,26 @@ return {
 		"nvim-neotest/neotest-python",
 	},
 
+	keys = {
+		{
+			"<leader>tt",
+			':lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
+			desc = "Run current tests file with neotest",
+		},
+		{
+			"<leader>tp",
+			':lua require("neotest").run.run(vim.fn.getcwd())<CR>',
+			desc = "Run tests in project with neotest",
+		},
+		{ "<leader>ts", ":Neotest summary<CR>", desc = "Show test summary" },
+		{ "<leader>to", ":Neotest output<CR>", desc = "Show test output pane" },
+	},
 	config = function()
 		require("neotest").setup({
 			adapters = {
-				require("neotest-python"),
+				require("neotest-python")({
+					python = ".venv/bin/python",
+				}),
 			},
 		})
 	end,
--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -11,6 +11,7 @@ return { -- Useful plugin to show you pending keybinds.
 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
 			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
 			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+			["<leader>t"] = { name = "[T]ests", _ = "which_key_ignore" },
 		})
 	end,
 }
