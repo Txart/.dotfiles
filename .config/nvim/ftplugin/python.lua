403a93be00a315b8c1d774640087a29e629f6e72
Txart <iurzainki@gmail.com>
2024-11-12 09:43:56 +0100

    folding for python

---
+++ .config/nvim/ftplugin/python.lua
@@ -0,0 +1,5 @@
+-- Enable folding for python files
+vim.opt_local.foldmethod = "indent" -- Use indent-based folding
+vim.opt_local.foldnestmax = 10 -- Maximum nesting level
+vim.opt_local.foldlevel = 1 -- Start with all folds open
+vim.opt_local.foldenable = true -- Enable folding
