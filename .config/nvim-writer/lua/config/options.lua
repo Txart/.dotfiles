5d190832fd030ac2bef9f2721aa6fd0756e5016f
txart <iurzainki@gmail.com>
2024-12-02 21:42:39 +0100

    improve nvimw

---
+++ .config/nvim-writer/lua/config/options.lua
@@ -0,0 +1,6 @@
+-- colorscheme
+vim.o.background = "light" -- This sets a light background
+vim.cmd("colorscheme default") -- Neovim's default light mode
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.mouse = "a"
