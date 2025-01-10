2008f3e49589f92b6ee001150cc8680f21efe363
txart <iurzainki@gmail.com>
2025-01-10 09:36:56 +0100

    fixed some whichkey warnings

--- .config/nvim/lua/plugins/whichkey.lua
+++ .config/nvim/lua/plugins/whichkey.lua
@@ -1,17 +1,48 @@
-return { -- Useful plugin to show you pending keybinds.
-	"folke/which-key.nvim",
-	event = "VimEnter", -- Sets the loading event to 'VimEnter'
-	config = function() -- This is the function that runs, AFTER loading
-		require("which-key").setup()
+-- return { -- Useful plugin to show you pending keybinds.
+-- 	"folke/which-key.nvim",
+-- 	event = "VimEnter", -- Sets the loading event to 'VimEnter'
+-- 	config = function() -- This is the function that runs, AFTER loading
+-- 		require("which-key").setup()
+--
+-- 		-- Document existing key chains
+-- 		require("which-key").register({
+-- 			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+-- 			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+-- 			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+-- 			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
+-- 			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+-- 			["<leader>t"] = { name = "[T]ests", _ = "which_key_ignore" },
+-- 		})
+-- 	end,
+-- }
 
-		-- Document existing key chains
-		require("which-key").register({
-			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-			["<leader>f"] = { name = "[F]ind", _ = "which_key_ignore" },
-			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-			["<leader>t"] = { name = "[T]ests", _ = "which_key_ignore" },
-		})
-	end,
+return {
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		spec = {
+
+			{ "<leader>c", group = "[C]ode" },
+			{ "<leader>c_", hidden = true },
+			{ "<leader>d", group = "[D]ocument" },
+			{ "<leader>d_", hidden = true },
+			{ "<leader>f", group = "[F]ind" },
+			{ "<leader>f_", hidden = true },
+			{ "<leader>r", group = "[R]ename" },
+			{ "<leader>r_", hidden = true },
+			{ "<leader>t", group = "[T]ests" },
+			{ "<leader>t_", hidden = true },
+			{ "<leader>w", group = "[W]orkspace" },
+			{ "<leader>w_", hidden = true },
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
