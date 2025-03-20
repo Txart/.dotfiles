e3605d45b6528f9608a255e11cb8a139c670007e
txart <iurzainki@gmail.com>
2025-03-20 13:09:31 +0100

    added nebula wallpaper

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -302,7 +302,7 @@ screens = [
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
-        wallpaper="/home/txart/.config/qtile/resources/hubble_deep_field.jpg",
+        wallpaper="/home/txart/.config/qtile/resources/jwst-carina-nebula.jpg",
     ),
 ]
 
