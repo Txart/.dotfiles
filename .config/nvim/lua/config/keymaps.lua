04137024fe3533e30e6bf9a750b6c3811ab2f04f
txart <iurzainki@gmail.com>
2025-08-10 06:49:45 +0200

    moved leader key config to right file

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,5 @@
 local keymap = vim.keymap.set
 
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 -- Escape with the RIGHT KEYS
 keymap("i", "kj", "<ESC>")
 keymap("v", "kj", "<ESC>")
