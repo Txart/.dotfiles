7b98a5549344ed759115d666faf40c4f74e4377d
txart <iurzainki@gmail.com>
2025-09-17 08:55:22 +0200

    slime + cells for rust too!

--- .config/nvim/lua/plugins/vim-slime-cells.lua
+++ .config/nvim/lua/plugins/vim-slime-cells.lua
@@ -2,6 +2,20 @@ return {
 	"klafyvel/vim-slime-cells",
 	requires = { { "jpalardy/vim-slime", opt = true } },
 	config = function()
+		-- Set cell delimiters based on file type
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "python",
+			callback = function()
+				vim.g.slime_cell_delimiter = "^\\s*# %%"
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "rust",
+			callback = function()
+				vim.g.slime_cell_delimiter = "^\\s*//%%"
+			end,
+		})
 		-- Key mappings
 		vim.api.nvim_set_keymap("n", "<leader>cv", "<Plug>SlimeConfig", {})
 		vim.api.nvim_set_keymap("n", "<leader>cc", "<Plug>SlimeCellsSend", {})
